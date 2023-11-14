da5320b680694a5e1bb41069cac988ae2c109d28
shanggeshihun <shanggeshihun@gmailcom>
2023-11-14 21:42:18 +0800

    新增musics文件夹，新增music_list.txt

--- TongZhong/download_by_music_name.py
+++ TongZhong/download_by_music_name.py
@@ -14,7 +14,7 @@ import time
 import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.alert import Alert
-    
+
 sys.setrecursionlimit(100000)
 driver_path = r"C:\Users\Administrator\AppData\Local\Google\Chrome\Application\chromedriver.exe"
 
@@ -23,7 +23,7 @@ home_page_url = 'https://tonzhon.com/music_for_commercial_use'
 driver_path = r"C:\Users\Administrator\AppData\Local\Google\Chrome\Application\chromedriver.exe"
 
 
-def  search_music(music_name):
+def search_music(music_name):
     '''
     :param music_name:搜索歌曲名称
     :return: 返回歌曲下载链接列表
@@ -41,7 +41,7 @@ def  search_music(music_name):
     })
 
     browser.get(home_page_url)
-    time.sleep(1)
+    time.sleep(2)
 
     current_window, current_url = browser.current_window_handle, browser.current_url
     print('查询页面-窗口信息：', current_window)
@@ -59,32 +59,38 @@ def  search_music(music_name):
     print('提交查询返回页面-当前窗口URL:', current_url)
 
     # 点击播放按钮
-    browser.find_element_by_xpath('//li[contains(@class,"song-item")]/div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]').click()
-    time.sleep(5)
+    browser.find_element_by_xpath(
+        '//li[contains(@class,"song-item")]/div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]').click()
+    time.sleep(3)
 
     current_window, current_url = browser.current_window_handle, browser.current_url
     print('播放页面-窗口信息：', current_window)
     print('播放页面-当前窗口URL:', current_url)
 
+    try:
+        mp3_url = browser.find_element_by_xpath('//div[@id="player"]/audio').get_attribute('src')
+    except Exception as e:
+        return
 
-    mp3_url = browser.find_element_by_xpath('//div[@id="player"]/audio').get_attribute('src')
-    print(mp3_url)
+    print('MP3地址:', mp3_url)
 
     browser.quit()
-    browser.close()
-    browser.service.stop()
+
     return mp3_url
 
-def download_music(music_name,mp3_url):
-    response = requests.get(url=mp3_url)
+
+def download_music(music_name, mp3_url):
+    response = requests.get(url=mp3_url, verify=False)
     if response.status_code == 200:
-        with open('{}.mp3'.format(music_name), "wb") as fp:
+        with open(r'./musics/{}.mp3'.format(music_name), "wb") as fp:
             fp.write(response.content)
         print("下载完成")
     else:
         print('无法下载')
+
+
 if __name__ == '__main__':
     music_name = '最熟悉的陌生人'
     mp3_url = search_music(music_name)
-    if mp3_url != '':
+    if mp3_url:
         download_music(music_name, mp3_url)
