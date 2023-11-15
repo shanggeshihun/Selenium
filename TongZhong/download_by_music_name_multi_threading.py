8813da3eb5c5ff3ddd91eed69c94a26202b6294f
shanggeshihun <shanggeshihun@gmailcom>
2023-11-16 00:38:39 +0800

    使用多线程-测试

---
+++ TongZhong/download_by_music_name_multi_threading.py
@@ -0,0 +1,199 @@
+# _*_coding:utf-8 _*_
+
+# @Time     :2023/11/15 7:24
+# @Author   :anliu
+# @File     :download_by_music_name.py
+# @Theme    :PyCharm
+
+import requests, time, os, warnings, threading
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from queue import Queue
+
+# 关闭特定的警告
+warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+
+# 常量定义
+DRIVER_PATH = r"C:\Users\Administrator\AppData\Local\Google\Chrome\Application\chromedriver.exe"
+HOME_PAGE_URL = 'https://tonzhon.com/music_for_commercial_use'
+MUSIC_LIST_FILE_PATH = r'./music_list.txt'
+MUSIC_DOWNLOAD_DIR = './musics'
+
+MUSIC_QUEUE_NOT_EMPTY = True
+MUSIC_URL_QUEUE_NOT_EMPTY = True
+
+thread_lock = threading.Lock()
+thread_write_lock = threading.Lock()
+
+class ZhongTongMp3UrlThread(threading.Thread):
+    def __init__(self, thread_id, music_name_queue, music_url_queue):
+        threading.Thread.__init__(self)
+        self.setName('Crawl' + self.name)
+        # 使用开发者模式
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        self.browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """
+            Object.defineProperty(navigator, 'webdriver', {
+              get: () => undefined
+            })
+          """
+        })
+
+        self.music_name_queue = music_name_queue
+        self.music_url_queue = music_url_queue
+
+    def run(self):
+        while MUSIC_QUEUE_NOT_EMPTY:
+            try:
+                thread_lock.acquire()
+                music_name = self.music_name_queue.get(False)
+                self.music_name_queue.task_done()
+                thread_lock.release()
+            except:
+                pass
+            else:
+                self.search_music(music_name)
+        self.browser.quit()
+
+    def search_music(self, music_name):
+        '''
+        搜索歌曲名称
+        :param music_name:搜索歌曲名称
+        :return: 返回歌曲下载链接列表
+        '''
+        self.browser.get(HOME_PAGE_URL)
+
+        # 输入歌名
+        try:
+            input_element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//input[@type="text"]'))
+            )
+            for m in music_name:
+                input_element.send_keys(m)
+                time.sleep(np.random.random())
+        except Exception as e:
+            print('输入歌名失败:', e)
+            self.browser.quit()
+            return None
+
+        # 提交查询
+        try:
+            submit_button = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//button[contains(@type,"button")]'))
+            )
+            submit_button.click()
+        except Exception as e:
+            print('提交查询失败:', e)
+            self.browser.quit()
+            return None
+
+
+        # 点击播放按钮
+        try:
+            play_button = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@class="masonry"]/div[contains(@class,"card search-result")][3]//li[contains(@class,"song-item")]//div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]'))
+            )
+        except Exception as e:
+            try:
+                self.browser.execute_script("window.scrollBy(0, 500);")
+                play_button = WebDriverWait(self.browser, 10).until(
+                    EC.presence_of_element_located((By.XPATH,'//div[@class="masonry"]/div[contains(@class,"card search-result")][4]//li[contains(@class,"song-item")]//div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]'))
+                )
+            except Exception as e:
+                print('\t点击播放失败:', e)
+                self.browser.quit()
+                return None
+            else:
+                play_button.click()
+        else:
+            play_button.click()
+
+        time.sleep(3)
+        # 获取MP3链接
+        try:
+            mp3_url = self.browser.find_element_by_xpath('//div[@id="player"]/audio').get_attribute('src')
+        except Exception as e:
+            print('\t获取MP3链接失败:', e)
+            self.browser.quit()
+            return None
+        else:
+            self.browser.quit()
+            self.music_url_queue.put((music_name, mp3_url))
+
+class DownloadMusicThread(threading.Thread):
+    def __init__(self, thread_id, music_url_queue):
+        threading.Thread.__init__(self)
+        self.setName('Parse' + self.name)
+        self.music_url_queue = music_url_queue
+
+    def run(self):
+        while MUSIC_URL_QUEUE_NOT_EMPTY:
+            try:
+                music_url = self.music_url_queue.get(False)
+                self.music_url_queue.task_done()
+            except:
+                pass
+            else:
+                self.download_music(music_url[0], music_url[1])
+
+    def download_music(self, music_name, mp3_url):
+        response = requests.get(url=mp3_url, verify=False)
+        if response.status_code == 200:
+            with open(os.path.join(MUSIC_DOWNLOAD_DIR, f'{music_name}.mp3'), "wb") as fp:
+                fp.write(response.content)
+        print(response.status_code)
+
+def main():
+    # 音乐名称队列
+    music_name_queue = Queue()
+    # 下载链接对接
+    music_url_queue = Queue()
+
+    with open(MUSIC_LIST_FILE_PATH, mode='r', encoding='utf-8') as f:
+        music_list = f.readlines()
+    for music in music_list:
+        music_name_queue.put(music.strip())
+
+    music_thread_name_list = ['music_thread_' + str(i) for i in range(1)]
+    music_thread_list = []
+    for thread_id in music_thread_name_list:
+        thread = ZhongTongMp3UrlThread(thread_id, music_name_queue, music_url_queue)
+        thread.start()
+        music_thread_list.append(thread)
+
+    download_thread_name_list = ['download_thread_' + str(i) for i in range(1)]
+    download_thread_list = []
+    for thread_id in download_thread_name_list:
+        thread = DownloadMusicThread(thread_id, music_url_queue)
+        thread.start()
+        download_thread_list.append(thread)
+
+
+    while not music_name_queue.empty():
+        pass
+    global MUSIC_QUEUE_NOT_EMPTY
+    MUSIC_QUEUE_NOT_EMPTY = False
+
+    for t in music_thread_list:
+        t.join()
+    print('爬虫程序已结束')
+
+    while not music_url_queue.empty():
+        pass
+    global MUSIC_URL_QUEUE_NOT_EMPTY
+    MUSIC_URL_QUEUE_NOT_EMPTY = False
+    for t in download_thread_list:
+        t.join()
+    print('下载程序已经结束')
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print(time.time() - start_time)
--- TongZhong/download_by_music_name_v1.py
+++ TongZhong/download_by_music_name_v1.py
@@ -5,14 +5,16 @@
 # @File     :download_by_music_name.py
 # @Theme    :PyCharm
 
-import requests
+import requests, time, os, warnings
+import numpy as np
 from selenium import webdriver
-import time
-import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+# 关闭特定的警告
+warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 
 # 常量定义
 DRIVER_PATH = r"C:\Users\Administrator\AppData\Local\Google\Chrome\Application\chromedriver.exe"
@@ -20,28 +22,37 @@ HOME_PAGE_URL = 'https://tonzhon.com/music_for_commercial_use'
 MUSIC_LIST_FILE_PATH = r'./music_list.txt'
 MUSIC_DOWNLOAD_DIR = './musics'
 
-# 配置ChromeDriver选项
-options = Options()
-options.add_experimental_option('excludeSwitches', ['enable-automation'])
-
-# 初始化webdriver
-browser = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
-
 def search_music(music_name):
     '''
     搜索歌曲名称
     :param music_name:搜索歌曲名称
     :return: 返回歌曲下载链接列表
     '''
+    # 使用开发者模式
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    browser = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+    browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                  get: () => undefined
+                })
+              """
+    })
+
     browser.get(HOME_PAGE_URL)
+
     # 输入歌名
     try:
         input_element = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.XPATH, '//input[@type="text"]'))
         )
-        input_element.send_keys(music_name)
+        for m in music_name:
+            input_element.send_keys(m)
+            time.sleep(np.random.random())
     except Exception as e:
         print('输入歌名失败:', e)
+        browser.quit()
         return None
 
     # 提交查询
@@ -52,32 +63,40 @@ def search_music(music_name):
         submit_button.click()
     except Exception as e:
         print('提交查询失败:', e)
+        browser.quit()
         return None
 
-    # 等待页面加载
-    time.sleep(5)
 
     # 点击播放按钮
     try:
         play_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//div[@class="masonry"]/div[contains(@class,"card search-result")][3]//li[contains(@class,"song-item")]//div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]'))
+            EC.presence_of_element_located((By.XPATH, '//div[@class="masonry"]/div[contains(@class,"card search-result")][3]//li[contains(@class,"song-item")]//div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]'))
         )
-        play_button.click()
     except Exception as e:
-        print('点击播放失败:', e)
-        return None
+        try:
+            play_button = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH,'//div[@class="masonry"]/div[contains(@class,"card search-result")][4]//li[contains(@class,"song-item")]//div[@class="ant-row ant-row-middle css-tx6cnl"]/div/div/div[@class="ant-space-item"]'))
+            )
+        except Exception as e:
+            print('\t点击播放失败:', e)
+            browser.quit()
+            return None
+        else:
+            play_button.click()
+    else:
+        play_button.click()
 
-    # 等待页面加载
-    time.sleep(5)
-
+    time.sleep(3)
     # 获取MP3链接
     try:
         mp3_url = browser.find_element_by_xpath('//div[@id="player"]/audio').get_attribute('src')
     except Exception as e:
-        print('获取MP3链接失败:', e)
+        print('\t获取MP3链接失败:', e)
+        browser.quit()
         return None
-
-    return mp3_url
+    else:
+        browser.quit()
+        return mp3_url
 
 def download_music(music_name, mp3_url):
     response = requests.get(url=mp3_url, verify=False)
@@ -95,18 +114,14 @@ if __name__ == '__main__':
     for music in musics_list:
         music_name = music.strip()
         print(f'----------------《{music_name}》正在下载----------------')
-        is_download = False
-        for _ in range(2):
-            mp3_url = search_music(music_name)
-            if not mp3_url:
-                continue
-            time.sleep(2)
-            else:
-                is_download = download_music(music_name, mp3_url)
 
-            if is_download:
-                break
+        is_download = False
+        mp3_url = search_music(music_name)
+        if not mp3_url:
+            time.sleep(2)
+            continue
+        else:
+            is_download = download_music(music_name, mp3_url)
+
         if not is_download:
             print('\t下载失败')
-
-    browser.quit()  # 退出浏览器
